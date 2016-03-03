e6174323029ad80ab769003cb71a6c3e548d85f6
George Fogle <fogle.george@gmail.com>
2016-03-03 00:25:52 -0500

    fix new eslint module/source type issue

--- src/lib.js
+++ src/lib.js
@@ -6,9 +6,7 @@ let vdom_instances = {};
 
 function guid() {
 	function s4() {
-		return Math.floor((1 + Math.random()) * 0x10000)
-		.toString(16)
-		.substring(1);
+		return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 	}
 	return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
 	s4() + '-' + s4() + s4() + s4();
