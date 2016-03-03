765833e2dbe563cd8169c43a1c36759e7bda2fe3
George Fogle <fogle.george@gmail.com>
2016-03-02 20:11:30 -0500

    basic register and mount

--- docs/src/docs.js
+++ docs/src/docs.js
@@ -1,3 +1,18 @@
 import TBD from "TBD";
 
-console.log(TBD);
+const { register, mount, div } = TBD;
+
+TBD.register('Root', {
+	actions: [],
+	render: function(state, props, actions){
+		return div(
+			{}, // styles
+			{}, // attrs
+			[
+				// children
+			]
+		);
+	}
+});
+
+TBD.mount('Root', 'root');
--- src/lib.js
+++ src/lib.js
@@ -1,3 +1,54 @@
-export default class TBD {
+let vdom_definitions = {};
+let action_definitions = {};
+let vdom_instances = {};
 
+function guid() {
+	function s4() {
+		return Math.floor((1 + Math.random()) * 0x10000)
+		.toString(16)
+		.substring(1);
+	}
+	return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+	s4() + '-' + s4() + s4() + s4();
+}
+
+module.exports = {
+	register: function(name, definition) {
+		if (vdom_definitions[name]) {
+			throw new Error('Cannot register definition more than once');
+		} else {
+			vdom_definitions[name] = function() {
+				return {
+					actions: definition.actions,
+					render: definition.render
+				};
+			};
+
+			return vdom_definitions[name];
+		}
+	},
+	mount: function(name, id) {
+		let defn = vdom_definitions[name];
+		let element = document.getElementById(id);
+		let component = new defn();
+		let uid = guid();
+
+		component.id = uid;
+		vdom_instances[uid] = component;
+
+		let htmlTree = component.render(
+			{}, // no state
+			{}, // no props
+			component.actions.reduce((acc, curr) => {
+				acc[curr] = action_definitions[curr];
+			}, {})
+		);
+
+		element.appendChild(htmlTree);
+	},
+	div: function(styles, attrs, children) {
+		let element = document.createElement("DIV");
+
+		return element;
+	}
 };
