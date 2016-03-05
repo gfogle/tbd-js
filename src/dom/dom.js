981504f381f5a1d7b1a8c60242d11be953d00c19
George Fogle <fogle.george@gmail.com>
2016-03-04 20:57:42 -0500

    refactor to helpers and component

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -1,8 +1,10 @@
 import perf from "../perf/perf";
+import helpers from "../helpers/helpers";
 
 let dom = {};
 
 function createDomElement(tag, styles, attrs, children) {
+	// TODO: use DOM Fragments instead?
 	let element = document.createElement(tag);
 
 	if (element instanceof HTMLUnknownElement) {
@@ -39,31 +41,7 @@ function createDomElement(tag, styles, attrs, children) {
 }
 
 function createValidTags() {
-	// TODO: recreate this list as a polyfill fetch on document
-	// see: http://www.w3schools.com/tags/
-	const validTags = [
-		'a', 'abbr', 'address', 'area', 'article', 'aside', 'audio',
-		'b', 'base', 'bdi', 'bdo', 'blockquote', 'body', 'br', 'button',
-		'canvas', 'caption', 'cite', 'code', 'col', 'colgroup',
-		'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'div', 'dl', 'dt',
-		'em', 'embed',
-		'fieldset', 'figcaption', 'figure', 'footer', 'form',
-		'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hr', 'html',
-		'i', 'iframe', 'img', 'input', 'ins',
-		'kbd', 'keygen',
-		'label', 'legend', 'li', 'link',
-		'main', 'map', 'mark', 'menu', 'menuitem', 'meta', 'meter',
-		'nav', 'nosript',
-		'object', 'ol', 'optgroup', 'option', 'output',
-		'p', 'param', 'pre', 'progress',
-		'q',
-		'rp', 'rt', 'ruby',
-		's','samp','script','section','select','small','source','span','strong','style','sub','summary','sup',
-		'table','tbody','td','textarea','tfoot','th','thead','time','title','tr','track',
-		'u','ul',
-		'var','video',
-		'wbr'
-	];
+	const validTags = helpers.getValidTags();
 
 	validTags.forEach((tag) => {
 		if (__DEV__) {
--- src/lib.js
+++ src/lib.js
@@ -1,38 +1,25 @@
 import dom from "./dom/dom";
 import perf from "./perf/perf";
+import Component from "./component/component";
 
 let vdom_definitions = {};
 let action_definitions = {};
 let vdom_instances = {};
 
-function guid() {
-	function s4() {
-		return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-	}
-	return s4() + s4() + '-' + s4() + '-' + s4() + '-' +s4() + '-' + s4() + s4() + s4();
-}
-
 function register(name, definition) {
 	if (vdom_definitions[name]) {
 		throw new Error('Cannot register definition more than once');
 	} else {
-		// TODO: do i need to save these definitions at all?
-		// TODO: should i be
-		vdom_definitions[name] = function() {
-			return {
-				id: guid(),
-				render: __DEV__ ? perf.instrument(name + "__render", definition.render) : definition.render
-			};
-		};
+		vdom_definitions[name] = definition;
 
-		return vdom_definitions[name]().render;
+		return vdom_definitions[name].render;
 	}
 }
 
 function mount(name, id) {
 	let defn = vdom_definitions[name];
 	let element = document.getElementById(id);
-	let component = new defn();
+	let component = new Component(name, defn);
 
 	vdom_instances[component.id] = component;
 
