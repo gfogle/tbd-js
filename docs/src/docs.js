a0887998ff30eed31071680f463c4b598c1b8aa8
George Fogle <fogle.george@gmail.com>
2016-03-05 14:33:04 -0500

    refactored to home component

--- docs/src/docs.js
+++ docs/src/docs.js
@@ -1,5 +1,6 @@
 import TBD from "TBD";
 import TopNav from "./top-nav/top-nav";
+import Home from "./home/home";
 
 const { register, mount } = TBD;
 const { div, nav, ul, li, span, button } = TBD.dom;
@@ -13,43 +14,6 @@ const styles = {
 		backgroundColor: '#303030',
 		invalid: 'fails?',
 		borderRadius: 'abc'
-	},
-	heroStyles: {
-		backgroundColor: '#FFD86E',
-		background: 'linear-gradient(#404040, #707070)',
-		minHeight: '300px',
-		marginTop: '55px',
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-	titleStyles: {
-		fontSize: '44px',
-		color: 'white'
-	},
-	subtitleStyles: {
-		fontSize: '18px',
-		color: 'white',
-		fontWeight: '300'
-	},
-	subtitleActionStyles: {
-		display: 'flex',
-		flexDirection: 'row',
-		flexWrap: 'nowrap',
-		paddingTop: '30px'
-	},
-	getStartedStyles: {
-		minWidth: '100px',
-		height: '50px',
-		fontSize: '16px',
-		cursor: 'pointer',
-		borderRadius: '2px',
-		border: 'none',
-		boxShadow: '1px 2px 2px #101010',
-		backgroundColor: '#ffffff',
-		padding: '0 15px',
-		outline: 'none'
 	}
 };
 
@@ -59,13 +23,7 @@ TBD.register('Root', {
 		return (
 			div(styles.divStyles, {}, [
 				TopNav(),
-				div(styles.heroStyles, {}, [
-					span(styles.titleStyles,{}, 'tbdJS'),
-					span(styles.subtitleStyles, {}, 'A JS FRAMEWORK FOR VIEWS, ROUTING, AND OTHER NONTRIVIAL THINGS'),
-					div(styles.subtitleActionStyles, {}, [
-						button(styles.getStartedStyles, {}, 'Get Started')
-					])
-				])
+				Home()
 			])
 		);
 	}
---
+++ docs/src/home/home.js
@@ -0,0 +1,66 @@
+import TBD from "TBD";
+
+const { register } = TBD;
+const { div, span, button } = TBD.dom;
+const styles = {
+	heroStyles: {
+		backgroundColor: '#FFD86E',
+		background: 'linear-gradient(#404040, #707070)',
+		minHeight: '300px',
+		marginTop: '55px',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	titleStyles: {
+		fontSize: '44px',
+		color: 'white'
+	},
+	subtitleStyles: {
+		fontSize: '18px',
+		color: 'white',
+		fontWeight: '300'
+	},
+	subtitleActionStyles: {
+		display: 'flex',
+		flexDirection: 'row',
+		flexWrap: 'nowrap',
+		paddingTop: '30px'
+	},
+	getStartedStyles: {
+		minWidth: '100px',
+		height: '50px',
+		fontSize: '16px',
+		cursor: 'pointer',
+		borderRadius: '2px',
+		border: 'none',
+		boxShadow: '1px 2px 2px #101010',
+		backgroundColor: '#ffffff',
+		padding: '0 15px',
+		outline: 'none'
+	}
+};
+const strings = {
+	title: 'tbdJS',
+	subtitle: 'A JS FRAMEWORK FOR VIEWS, ROUTING, AND OTHER NONTRIVIAL THINGS',
+	getStarted: 'Get Started'
+};
+
+let Home = TBD.register('Home', {
+	render: function(state, props, actions) {
+		return (
+			div({}, {}, [
+				div(styles.heroStyles, {}, [
+					span(styles.titleStyles,{}, strings.title),
+					span(styles.subtitleStyles, {}, strings.subtitle),
+					div(styles.subtitleActionStyles, {}, [
+						button(styles.getStartedStyles, {}, strings.getStarted)
+					])
+				])
+			])
+		);
+	}
+});
+
+export default Home;
